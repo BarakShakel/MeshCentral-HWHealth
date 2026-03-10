fc41e8829bfc63a1279acd0f0a96379ff8dc8727
BarakShakel <65956566+BarakShakel@users.noreply.github.com>
2026-03-10 21:24:16 +0200

    Update hwhealth.js

--- modules_meshcore/hwhealth.js
+++ modules_meshcore/hwhealth.js
@@ -1,13 +1,36 @@
 var exports = module.exports;
 
-exports.getHardwareHealth = function() {
+// This is just a placeholder for future real hardware collection.
+// You can later add PowerShell / WMIC / platform-specific collection here.
+exports.getHardwareHealth = function () {
+    try {
+        var os = require('os');
 
-    var data = "Hello from the Remote Agent! Ready to scan hardware.";
+        var data = {
+            hostname: os.hostname(),
+            platform: os.platform(),
+            arch: os.arch(),
+            uptimeSeconds: os.uptime(),
+            totalMemory: os.totalmem(),
+            freeMemory: os.freemem(),
+            cpus: os.cpus() ? os.cpus().length : 0,
+            note: 'Basic agent-side data collected successfully.'
+        };
 
-    require('meshcore').send({ 
-        action: 'plugin', 
-        plugin: 'hwhealth', 
-        method: 'healthData', 
-        data: data 
-    });
+        require('meshcore').send({
+            action: 'plugin',
+            plugin: 'hwhealth',
+            pluginaction: 'healthData',
+            data: data
+        });
+    } catch (ex) {
+        require('meshcore').send({
+            action: 'plugin',
+            plugin: 'hwhealth',
+            pluginaction: 'healthData',
+            data: {
+                error: ex.toString()
+            }
+        });
+    }
 };
