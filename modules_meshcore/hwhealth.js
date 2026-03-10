457e2d158f898ceb595c2791e8d2ac419c424183
BarakShakel <65956566+BarakShakel@users.noreply.github.com>
2026-03-10 23:53:09 +0200

    Update hwhealth.js
    Fixed

--- modules_meshcore/hwhealth.js
+++ modules_meshcore/hwhealth.js
@@ -1,157 +1,84 @@
-var exports = module.exports;
-var child_process = require('child_process');
+var child = require('child_process');
+var meshcore = require('meshcore');
 
-function execPowerShell(script, callback) {
-    try {
-        var args = [
-            '-NoLogo',
-            '-NoProfile',
-            '-NonInteractive',
-            '-Command',
-            script
-        ];
-
-        child_process.execFile('powershell.exe', args, {
-            windowsHide: true,
-            timeout: 30000,
-            maxBuffer: 1024 * 1024 * 4
-        }, function (err, stdout, stderr) {
-            if (err) {
-                callback(new Error((stderr || err.message || '').toString().trim()));
-                return;
-            }
-            callback(null, stdout);
-        });
-    } catch (ex) {
-        callback(ex);
-    }
+function runPS(cmd, cb) {
+    child.execFile(
+        'powershell.exe',
+        ['-NoProfile','-NonInteractive','-Command',cmd],
+        { windowsHide: true, timeout: 15000 },
+        function(err, stdout) {
+            if (err) { cb(err); return; }
+            cb(null, stdout.trim());
+        }
+    );
 }
 
-exports.getHardwareHealth = function (sessionid, nodeid) {
+function collect(sessionid,nodeid) {
+
     var ps = `
-$ErrorActionPreference = 'SilentlyContinue'
+$cpuTemp = (Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace root/wmi |
+Select-Object -First 1).CurrentTemperature
 
-function To-GB([UInt64]$v) {
-    if ($null -eq $v) { return $null }
-    return [Math]::Round($v / 1GB, 2)
+if($cpuTemp){
+ $cpuTemp=[math]::Round(($cpuTemp/10)-273.15,1)
 }
 
-$cs   = Get-CimInstance Win32_ComputerSystem
-$bios = Get-CimInstance Win32_BIOS
-$cpu  = Get-CimInstance Win32_Processor | Select-Object -First 1
-$os   = Get-CimInstance Win32_OperatingSystem
-$batt = Get-CimInstance Win32_Battery
-$ld   = Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3"
+$cpuLoad = (Get-CimInstance Win32_Processor |
+Measure-Object LoadPercentage -Average).Average
 
-$physicalDisks = @()
-if (Get-Command Get-PhysicalDisk -ErrorAction SilentlyContinue) {
-    $physicalDisks = Get-PhysicalDisk | Select-Object FriendlyName, MediaType, HealthStatus, OperationalStatus, Size, SerialNumber
+$ram = Get-CimInstance Win32_OperatingSystem
+
+$result = @{
+ cpuTemp=$cpuTemp
+ cpuLoad=$cpuLoad
+ ramUsed=[math]::Round(($ram.TotalVisibleMemorySize-$ram.FreePhysicalMemory)/1MB,2)
+ ramTotal=[math]::Round($ram.TotalVisibleMemorySize/1MB,2)
 }
 
-$smart = @()
-try {
-    $smart = Get-CimInstance -Namespace root\\wmi -ClassName MSStorageDriver_FailurePredictStatus |
-        Select-Object InstanceName, PredictFailure, Reason
-} catch {}
-
-$result = [ordered]@{
-    collectedAt     = (Get-Date).ToString("s")
-    computerName    = $env:COMPUTERNAME
-    manufacturer    = $cs.Manufacturer
-    model           = $cs.Model
-    serialNumber    = $bios.SerialNumber
-    biosVersion     = (($bios.SMBIOSBIOSVersion, $bios.Version | Where-Object { $_ }) -join " / ")
-    cpuName         = $cpu.Name
-    cpuCores        = $cpu.NumberOfCores
-    cpuLogical      = $cpu.NumberOfLogicalProcessors
-    totalMemoryGB   = To-GB $cs.TotalPhysicalMemory
-    freeMemoryGB    = To-GB ($os.FreePhysicalMemory * 1KB)
-    memorySummary   = ("{0} GB total / {1} GB free" -f (To-GB $cs.TotalPhysicalMemory), (To-GB ($os.FreePhysicalMemory * 1KB)))
-    batterySummary  = if ($batt) {
-        (($batt | ForEach-Object {
-            $status = $_.BatteryStatus
-            $charge = $_.EstimatedChargeRemaining
-            "Status=$status Charge=$charge%"
-        }) -join "; ")
-    } else {
-        "No battery"
-    }
-    logicalDisks    = @($ld | ForEach-Object {
-        [ordered]@{
-            DeviceID    = $_.DeviceID
-            VolumeName  = $_.VolumeName
-            FileSystem  = $_.FileSystem
-            SizeGB      = To-GB $_.Size
-            FreeGB      = To-GB $_.FreeSpace
-        }
-    })
-    physicalDisks   = @($physicalDisks | ForEach-Object {
-        [ordered]@{
-            FriendlyName       = $_.FriendlyName
-            MediaType          = $_.MediaType
-            HealthStatus       = $_.HealthStatus
-            OperationalStatus  = (($_.OperationalStatus | Out-String).Trim())
-            SizeGB             = To-GB $_.Size
-            SerialNumber       = $_.SerialNumber
-        }
-    })
-    smartStatus     = @($smart | ForEach-Object {
-        [ordered]@{
-            InstanceName    = $_.InstanceName
-            PredictFailure  = $_.PredictFailure
-            Reason          = $_.Reason
-        }
-    })
-}
-
-$result | ConvertTo-Json -Depth 6 -Compress
+$result | ConvertTo-Json -Compress
 `;
 
-    execPowerShell(ps, function (err, stdout) {
-        if (err) {
-            require('meshcore').send({
-                action: 'plugin',
-                plugin: 'hwhealth',
-                pluginaction: 'healthError',
-                sessionid: sessionid,
-                nodeid: nodeid,
-                message: 'PowerShell collection failed: ' + err.message
+    runPS(ps,function(err,out){
+
+        if(err){
+            meshcore.send({
+                action:'plugin',
+                plugin:'hwhealth',
+                pluginaction:'healthError',
+                sessionid:sessionid,
+                nodeid:nodeid,
+                message:err.toString()
             });
             return;
         }
 
-        try {
-            var data = JSON.parse(stdout);
-            require('meshcore').send({
-                action: 'plugin',
-                plugin: 'hwhealth',
-                pluginaction: 'healthData',
-                sessionid: sessionid,
-                nodeid: nodeid,
-                data: data
-            });
-        } catch (ex) {
-            require('meshcore').send({
-                action: 'plugin',
-                plugin: 'hwhealth',
-                pluginaction: 'healthError',
-                sessionid: sessionid,
-                nodeid: nodeid,
-                message: 'JSON parse failed: ' + ex.toString() + ' | Raw output: ' + stdout
-            });
+        var data=null;
+        try{
+            data=JSON.parse(out);
+        }catch(e){
+            data={raw:out};
         }
+
+        meshcore.send({
+            action:'plugin',
+            plugin:'hwhealth',
+            pluginaction:'healthData',
+            sessionid:sessionid,
+            nodeid:nodeid,
+            data:data
+        });
+
     });
-};
+}
 
-function handleCommand(cmd) {
-    if (!cmd || cmd.action !== 'plugin' || cmd.plugin !== 'hwhealth') return;
-    if (cmd.pluginaction === 'getHealth') {
-        exports.getHardwareHealth(cmd.sessionid, cmd.nodeid);
+exports.processMessage=function(msg){
+
+    if(!msg) return;
+    if(msg.action!='plugin') return;
+    if(msg.plugin!='hwhealth') return;
+
+    if(msg.pluginaction=='getHealth'){
+        collect(msg.sessionid,msg.nodeid);
     }
-}
 
-try {
-    require('meshcore').on('message', handleCommand);
-} catch (e) {
-    // Fallback if event handler model differs
-}
+};
